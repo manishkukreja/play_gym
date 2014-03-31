b8d257108b370ecba5b29a105da5856cf06b3ed3
pravin vaja <vajapravin23@gmail.com>
2014-03-31 04:52:30 -0400

    authenticate user on members create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def authenticate_admin
+      if current_user.admin
+        flash[:error] = "You don't have priviliges to access this page!";
+        redirect_to root_path
+      end
+  end
 end
--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,6 @@
 class MembersController < ApplicationController
+    before_filter :authenticate_user!
+    before_filter :authenticate_admin
   # GET /members
   # GET /members.json
   def index
