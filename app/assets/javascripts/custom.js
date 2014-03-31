4916c2a186b5ac042c89fcd6c86cb7fc23ace709
pravin vaja <vajapravin23@gmail.com>
2014-03-31 10:43:30 -0400

    precompiled

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -34,27 +34,6 @@
           handler.fitMapToBounds();
         });
         
-        $('input[type=radio][name=football]').change(function(){
-          $("#football").html($('input[type=radio][name=football]:checked').val());
-        });
-        $('input[type=radio][name=tennis]').change(function(){
-          $("#tennis").html($('input[type=radio][name=tennis]:checked').val());
-        });
-        $('input[type=radio][name=yoga]').change(function(){
-          $("#yoga").html($('input[type=radio][name=yoga]:checked').val());
-        });
-        $('input[type=radio][name=karate]').change(function(){
-          $("#karate").html($('input[type=radio][name=karate]:checked').val());
-        });
-        $('input[type=radio][name=gymnastics]').change(function(){
-          $("#gymnatics").html($('input[type=radio][name=gymnastics]:checked').val());
-        });
-        $('input[type=radio][name=art_craft]').change(function(){
-          $("#art_craft").html($('input[type=radio][name=art_craft]:checked').val());
-        });
-        $('input[type=radio][name=water_play]').change(function(){
-          $("#water_play").html($('input[type=radio][name=water_play]:checked').val());
-        });
     });
 
   function checkForm(start_date, end_date)
