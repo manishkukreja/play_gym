f554d7d64b992e23087afd894998ad7293ffe772
Pravinkumar Vaja <pravinkumar.vaja@synechron.com>
2014-04-07 13:17:06 +0530

    member_activities model already exists -- migration error

--- db/migrate/20140331100149_create_member_activities.rb
+++ db/migrate/20140331100149_create_member_activities.rb
@@ -1,5 +1,6 @@
 class CreateMemberActivities < ActiveRecord::Migration
   def change
+  	drop_table :member_activities
     create_table :member_activities do |t|
       t.references :member, index: true
       t.references :activity, index: true
